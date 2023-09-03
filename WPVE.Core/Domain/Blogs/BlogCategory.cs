d7d4e21c5ef07c611befa18205e50e26bd05c543
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-03 18:57:15 +0330

    Create BlogCategory

--- WPVE.Core/Domain/Blogs/BlogCategory.cs
+++ WPVE.Core/Domain/Blogs/BlogCategory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WPVE.Core.Domain.Blogs
 {
     public class BlogCategory:BaseEntity
@@ -6,17 +8,21 @@ namespace WPVE.Core.Domain.Blogs
         public BlogCategory()
         {
         }
+
         /// <summary>
         /// Gets or sets category title
         /// </summary>
+        [Display(Name ="عنوان")]
         public string Title { get; set; }
         /// <summary>
         ///  Gets or sets category description
         /// </summary>
+        [Display(Name = "توضیحات")]
         public string Description { get; set; }
         /// <summary>
         /// Gets or sets category Parent identifier
         /// </summary>
+        [Display(Name = "گروه پدر")]
         public string ParentId { get; set; }
     }
 }
---
+++ WPVE.Web/Areas/Admin/Controllers/BlogCategoryController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WPVE.Core.Domain.Blogs;
+using WPVE.Data;
+
+namespace WPVE.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class BlogCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogCategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/BlogCategory
+        public async Task<IActionResult> Index()
+        {
+              return _context.blogCategories != null ? 
+                          View(await _context.blogCategories.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.blogCategories'  is null.");
+        }
+
+  
+        // GET: Admin/BlogCategory/Create
+        public async Task<IActionResult> Create()
+        {
+            var categories =await _context.blogCategories.ToListAsync();
+            ViewData["ParentId"] = new SelectList(categories, "Id", "Title");
+
+            return View();
+        }
+
+        // POST: Admin/BlogCategory/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BlogCategory blogCategory)
+        {
+            try
+            {
+                blogCategory.Id = Guid.NewGuid().ToString();
+                blogCategory.CreatedOnUtc = DateTime.Now;
+                blogCategory.IPAddress = "";
+                if (blogCategory.ParentId == null)
+                {
+                    blogCategory.ParentId = Guid.Empty.ToString();
+                }
+                _context.Add(blogCategory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+              
+            
+            var categories = await _context.blogCategories.ToListAsync();
+            ViewData["ParentId"] = new SelectList(categories, "Id", "Title");
+            return View(blogCategory);
+        }
+
+        // GET: Admin/BlogCategory/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.blogCategories == null)
+            {
+                return NotFound();
+            }
+
+            var blogCategory = await _context.blogCategories.FindAsync(id);
+            if (blogCategory == null)
+            {
+                return NotFound();
+            }
+            return View(blogCategory);
+        }
+
+        // POST: Admin/BlogCategory/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Title,Description,ParentId,Id,CreatedOnUtc,IPAddress")] BlogCategory blogCategory)
+        {
+            if (id != blogCategory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(blogCategory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BlogCategoryExists(blogCategory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogCategory);
+        }
+
+        // GET: Admin/BlogCategory/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.blogCategories == null)
+            {
+                return NotFound();
+            }
+
+            var blogCategory = await _context.blogCategories
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (blogCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(blogCategory);
+        }
+
+        // POST: Admin/BlogCategory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.blogCategories == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.blogCategories'  is null.");
+            }
+            var blogCategory = await _context.blogCategories.FindAsync(id);
+            if (blogCategory != null)
+            {
+                _context.blogCategories.Remove(blogCategory);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BlogCategoryExists(string id)
+        {
+          return (_context.blogCategories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
