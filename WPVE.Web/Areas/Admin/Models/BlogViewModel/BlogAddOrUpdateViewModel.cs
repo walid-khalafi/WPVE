d4e2fe714037626a293351ee06386503d09493fe
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-04 08:24:00 +0330

    blogpost new design

--- WPVE.Web/Areas/Admin/Models/BlogViewModel/BlogAddOrUpdateViewModel.cs
+++ WPVE.Web/Areas/Admin/Models/BlogViewModel/BlogAddOrUpdateViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace WPVE.Web.Areas.Admin.Models.BlogViewModel
 {
     public class BlogAddOrUpdateViewModel
@@ -13,35 +15,47 @@ namespace WPVE.Web.Areas.Admin.Models.BlogViewModel
         /// <summary>
         /// Gets or sets the blog post title
         /// </summary>
+        [Display(Name = "عنوان")]
         public string Title { get; set; }
 
         /// <summary>
         /// Gets or sets the blog post body
         /// </summary>
+        [Display(Name = "متن اصلی")]
         public string Body { get; set; }
 
         /// <summary>
         /// Gets or sets the blog post overview. If specified, then it's used on the blog page instead of the "Body"
         /// </summary>
+        [Display(Name = "خلاصه متن")]
         public string BodyOverview { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the blog post comments are allowed 
         /// </summary>
+        [Display(Name = "اجازه ثبت نظر")]
         public bool AllowComments { get; set; }
+        /// <summary>
+        /// Gets or sets the value indicating whether this blog post should be included in sitemap
+        /// </summary>
+        [Display(Name = "نمایش در sitemap")]
+        public bool IncludeInSitemap { get; set; }
         /// <summary>
         /// Gets or sets the blog tags
         /// </summary>
+        [Display(Name = "تگ ها")]
         public string Tags { get; set; }
 
         /// <summary>
         /// Gets or sets the blog post start date and time
         /// </summary>
+        [Display(Name = "شروع انتشار")]
         public DateTime? StartDateUtc { get; set; }
 
         /// <summary>
         /// Gets or sets the blog post end date and time
         /// </summary>
+        [Display(Name = "پایان انتشار")]
         public DateTime? EndDateUtc { get; set; }
 
         /// <summary>
