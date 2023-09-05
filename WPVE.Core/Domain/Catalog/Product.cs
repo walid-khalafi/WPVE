a56cf7e4bbc74731d4396f5d92ea9224bae77515
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-05 11:27:01 +0330

    Create Product.cs

---
+++ WPVE.Core/Domain/Catalog/Product.cs
@@ -0,0 +1,156 @@
+﻿using System;
+namespace WPVE.Core.Domain.Catalog
+{
+    public class Product: BaseEntity
+    {
+        public Product()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the short description
+        /// </summary>
+        public string ShortDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full description
+        /// </summary>
+        public string FullDescription { get; set; }
+
+        /// <summary>
+        /// Gets or sets the  product categories identifiers
+        /// </summary>
+        public string ProductCategories { get; set; }
+
+        /// <summary>
+        /// Gets or sets the tax category identifier
+        /// </summary>
+        public int TaxCategoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Manufacturer  identifier
+        /// </summary>
+        public string ManufacturerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the stock quantity
+        /// </summary>
+        public int StockQuantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order minimum quantity
+        /// </summary>
+        public int OrderMinimumQuantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order maximum quantity
+        /// </summary>
+        public int OrderMaximumQuantity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the price
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start date and time of the new product (set product as "New" from date). Leave empty to ignore this property
+        /// </summary>
+        public DateTime? MarkAsNewStartDateTimeUtc { get; set; }
+        /// <summary>
+        /// Gets or sets the end date and time of the new product (set product as "New" to date). Leave empty to ignore this property
+        /// </summary>
+        public DateTime? MarkAsNewEndDateTimeUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the available start date and time
+        /// </summary>
+        public DateTime? AvailableStartDateTimeUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the available end date and time
+        /// </summary>
+        public DateTime? AvailableEndDateTimeUtc { get; set; }
+        /// <summary>
+        /// Gets or sets the weight
+        /// </summary>
+        public decimal Weight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the length
+        /// </summary>
+        public decimal Length { get; set; }
+
+        /// <summary>
+        /// Gets or sets the width
+        /// </summary>
+        public decimal Width { get; set; }
+
+        /// <summary>
+        /// Gets or sets the height
+        /// </summary>
+        public decimal Height { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to show the product on home page
+        /// </summary>
+        public bool ShowOnHomepage { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the product is marked as tax exempt
+        /// </summary>
+        public bool IsTaxExempt { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether this product is returnable (a customer is allowed to submit return request with this product)
+        /// </summary>
+        public bool NotReturnable { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to disable buy (Add to cart) button
+        /// </summary>
+        public bool DisableBuyButton { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this item is available for Pre-Order
+        /// </summary>
+        public bool AvailableForPreOrder { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether to show "Call for Pricing" or "Call for quote" instead of price
+        /// </summary>
+        public bool CallForPrice { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether this product is marked as new
+        /// </summary>
+        public bool MarkAsNew { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity is published
+        /// </summary>
+        public bool Published { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity has been deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets the meta title
+        /// </summary>
+        public string MetaTitle { get; set; }
+        /// <summary>
+        /// Gets or sets the meta keywords
+        /// </summary>
+        public string MetaKeywords { get; set; }
+        /// <summary>
+        /// Gets or sets the meta description
+        /// </summary>
+        public string MetaDescription { get; set; }
+
+
+
+    }
+}
+
