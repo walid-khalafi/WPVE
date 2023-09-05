af95002c30851ee3633d68d749b42587c826367d
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-05 11:27:08 +0330

    Update BaseEntity.cs

--- WPVE.Core/BaseEntity.cs
+++ WPVE.Core/BaseEntity.cs
@@ -11,12 +11,14 @@ namespace WPVE.Core
         /// </summary>
         [Key]
         public string Id { get; set; }
-
         /// <summary>
         /// Gets or sets the date and time of instance creation
         /// </summary>
         public DateTime CreatedOnUtc { get; set; }
-
+        /// <summary>
+        /// Gets or sets the date and time of instance update
+        /// </summary>
+        public DateTime UpdatedOnUtc { get; set; }
         /// <summary>
         /// Get or sets the IP Address
         /// </summary>
