ee106708994361604bf261bf78e82875f7db3809
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-07 21:03:56 +0330

    Create 20230907173118_edit_tag_name.cs

---
+++ WPVE.Data/Migrations/20230907173118_edit_tag_name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WPVE.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class edit_tag_name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Naame",
+                table: "ProductTags",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "ProductTags",
+                newName: "Naame");
+        }
+    }
+}
