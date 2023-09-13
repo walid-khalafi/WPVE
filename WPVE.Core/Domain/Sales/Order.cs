b806b2a0f3aa8901f91640d44f3a3b97a2f7e0f5
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-13 17:33:29 +0330

    Update Order.cs

--- WPVE.Core/Domain/Sales/Order.cs
+++ WPVE.Core/Domain/Sales/Order.cs
@@ -6,6 +6,82 @@ namespace WPVE.Core.Domain.Sales
         public Order()
         {
         }
+        /// <summary>
+        /// Gets or sets the order identifier
+        /// </summary>
+        public Guid OrderGuid { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the customer identifier
+        /// </summary>
+        public int CustomerId { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the pickup address identifier
+        /// </summary>
+        public int? PickupAddressId { get; set; }
+        /// <summary>
+        /// Gets or sets an order status identifier
+        /// </summary>
+        public int OrderStatusId { get; set; }
+        /// <summary>
+        /// Gets or sets the shipping status identifier
+        /// </summary>
+        public int ShippingStatusId { get; set; }
+        /// <summary>
+        /// Gets or sets the payment status identifier
+        /// </summary>
+        public int PaymentStatusId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the payment method system name
+        /// </summary>
+        public string PaymentMethodSystemName { get; set; }
+        /// <summary>
+        /// Gets or sets the tax rates
+        /// </summary>
+        public string TaxRates { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order tax
+        /// </summary>
+        public decimal OrderTax { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order discount (applied to order total)
+        /// </summary>
+        public decimal OrderDiscount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the order total
+        /// </summary>
+        public decimal OrderTotal { get; set; }
+        /// <summary>
+        /// Gets or sets the authorization transaction code
+        /// </summary>
+        public string AuthorizationTransactionCode { get; set; }
+        /// <summary>
+        /// Gets or sets the authorization transaction result
+        /// </summary>
+        public string AuthorizationTransactionResult { get; set; }
+        /// <summary>
+        /// Gets or sets the paid date and time
+        /// </summary>
+        public DateTime? PaidDateUtc { get; set; }
+        /// <summary>
+        /// Gets or sets the shipping method
+        /// </summary>
+        public string ShippingMethod { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the entity has been deleted
+        /// </summary>
+        public bool Deleted { get; set; }
+
+
+
 
     }
 }
