c7bf5f7ebca17d7654e6193c034d9cb2781c6e04
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-14 01:34:44 +0330

    Update ReturnRequest.cs

--- WPVE.Core/Domain/Orders/ReturnRequest.cs
+++ WPVE.Core/Domain/Orders/ReturnRequest.cs
@@ -1,10 +1,54 @@
 ﻿using System;
 namespace WPVE.Core.Domain.Orders
 {
-    public class ReturnRequest
+    public class ReturnRequest:BaseEntity
     {
         public ReturnRequest()
         {
+        }
+
+        /// <summary>
+        /// Gets or sets the order item identifier
+        /// </summary>
+        public int OrderItemId { get; set; }
+        /// <summary>
+        /// Gets or sets the customer identifier
+        /// </summary>
+        public string CustomerId { get; set; }
+        /// <summary>
+        /// Gets or sets the customer comments
+        /// </summary>
+        public string CustomerComments { get; set; }
+        /// <summary>
+        /// Gets or sets the quantity returned to stock
+        /// </summary>
+        public int ReturnedQuantity { get; set; }
+        /// <summary>
+        /// Gets or sets the reason to return
+        /// </summary>
+        public string ReasonForReturn { get; set; }
+
+        /// <summary>
+        /// Gets or sets the return status identifier
+        /// </summary>
+        public int ReturnRequestStatusId { get; set; }
+        /// <summary>
+        /// Gets or sets the date and time of entity creation
+        /// </summary>
+        public DateTime CreatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time of entity update
+        /// </summary>
+        public DateTime? UpdatedOnUtc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the return status
+        /// </summary>
+        public ReturnRequestStatus ReturnRequestStatus
+        {
+            get => (ReturnRequestStatus)ReturnRequestStatusId;
+            set => ReturnRequestStatusId = (int)value;
         }
     }
 }
