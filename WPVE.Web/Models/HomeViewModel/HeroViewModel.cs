126eb52d754987a24ed296a7e704d90f4e026159
Walid Khalafi <walid.khalafi@icloud.com>
2023-09-16 03:55:15 +0330

    Update HeroViewModel.cs

--- WPVE.Web/Models/HomeViewModel/HeroViewModel.cs
+++ WPVE.Web/Models/HomeViewModel/HeroViewModel.cs
@@ -11,9 +11,40 @@ namespace WPVE.Web.Models.HomeViewModel
         /// </summary>
         public string Name { get; set; }
         public DefaultHero DefaultHero { get; set; }
+        public VideoCallHero VideoCallHero { get; set; }
+        public TravelHero TravelHero { get; set; }
+        public TaskmaMagementHero TaskmaMagementHero { get; set; }
+        public StudioHero StudioHero { get; set; }
+        public SoftwareHero SoftwareHero { get; set; }
+        public SocialMarketingHero SocialMarketingHero { get; set; }
+        public SingleProductHero SingleProductHero { get; set; }
+        public ShopHero ShopHero { get; set; }
+        public ServicesHero ServicesHero { get; set; }
+        public SeoAgencyHero SeoAgencyHero { get; set; }
+        public SassHero SassHero { get; set; }
+        public RealEstateHero RealEstateHero { get; set; }
+        public PortfolioHero PortfolioHero { get; set; }
+        public PersonalHero PersonalHero { get; set; }
+        public OnlineLearningHero OnlineLearningHero { get; set; }
+        public MarketingHero MarketingHero { get; set; }
+        public LandingOneHero LandingOneHero { get; set; }
+        public LandingTwoHero LandingTwoHero { get; set; }
+        public LandingThreeHero LandingThreeHero { get; set; }
+        public LandingFourHero LandingFourHero { get; set; }
+        public IntegrationHero IntegrationHero { get; set; }
+        public HospitalHero HospitalHero { get; set; }
+        public FreelancerHero FreelancerHero { get; set; }
+        public EnterpriseHero EnterpriseHero { get; set; }
+        public DigitalAgencyHero DigitalAgencyHero { get; set; }
+        public DeveloperHero DeveloperHero { get; set; }
+        public CryptoHero CryptoHero { get; set; }
+        public CoworkingHero CoworkingHero { get; set; }
+        public BlogHero BlogHero { get; set; }
     }
 
-    public class DefaultHero {
+
+    public class DefaultHero
+    {
 
         /// <summary>
         /// Get or set Heading
@@ -61,5 +92,91 @@ namespace WPVE.Web.Models.HomeViewModel
         public string Button2Url { get; set; }
 
     }
+    public class VideoCallHero
+    {
+    }
+    public class TravelHero
+    {
+    }
+    public class TaskmaMagementHero
+    {
+    }
+    public class StudioHero
+    {
+    }
+    public class SoftwareHero
+    {
+    }
+    public class SocialMarketingHero
+    {
+    }
+    public class SingleProductHero
+    {
+    }
+    public class ShopHero
+    {
+    }
+    public class ServicesHero
+    {
+    }
+    public class SeoAgencyHero
+    {
+    }
+    public class SassHero
+    {
+    }
+    public class RealEstateHero
+    {
+    }
+    public class PortfolioHero
+    {
+    }
+    public class PersonalHero
+    {
+    }
+    public class OnlineLearningHero
+    {
+    }
+    public class MarketingHero
+    {
+    }
+    public class LandingOneHero
+    {
+    }
+    public class LandingTwoHero
+    {
+    }
+    public class LandingThreeHero
+    {
+    }
+    public class LandingFourHero
+    {
+    }
+    public class IntegrationHero
+    {
+    }
+    public class HospitalHero
+    {
+    }
+    public class FreelancerHero
+    {
+    }
+    public class EnterpriseHero
+    {
+    }
+    public class DigitalAgencyHero
+    {
+    }
+    public class DeveloperHero
+    {
+    }
+    public class CryptoHero
+    {
+    }
+    public class CoworkingHero
+    {
+    }
+    public class BlogHero
+    {
+    }
 }
-
